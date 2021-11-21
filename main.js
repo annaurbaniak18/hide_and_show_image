a791b4c96f69165c70831b9b7b6f46278c8fc543
Anna <annaurbaniak18@gmail.com>
2021-11-21 12:19:35 +0100

    hide and show image

---
+++ main.js
@@ -0,0 +1,14 @@
+const arrowIcon = document.querySelector('.fas')
+const btn = document.querySelector('.arrow')
+const img = document.querySelector('.item1')
+function showImage() {
+	img.classList.toggle('show')
+
+	if (img.classList.contains('show')) {
+		arrowIcon.style.transform = 'rotate(180deg)'
+	} else {
+		arrowIcon.style.transform='rotate(0)'
+	}
+}
+
+btn.addEventListener('click', showImage)
